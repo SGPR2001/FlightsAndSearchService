5ba04d1cfa3c6392d46f003b15720432855d60bb
SGPR2001 <sgddsp@gmail.com>
2024-01-03 08:54:51 +0530

    Completed  city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,19 +6,46 @@ class CityRepository{
         const city=await City.create({name});
         return city;
        }catch (error){
+        console.log("something went wrong in the repository layer")
         throw {error};
        }
     }
-    async deleteCity({ cityId }){
+    async deleteCity( cityId ){
         try{
               await City.destroy({
                 where:{
                     id:cityId
                 }
               })
+              return true;
         }catch(error){
+            console.log("something went wrong in the repository layer");
             throw{error}
         }
+    }
+
+    async updateCity(cityId,data){
+        try{
+            const city=await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            })
+            return city;
+        }catch(error){
+            console.log("something went wrong in the repository layer");
+            throw{error}
+        }
+    }
+    async getCity(cityId){
+        try{
+            const city=await City.findByPk(cityId)
+            return City;
+        }catch(error){
+            console.log("something went wrong in the repository layer");
+            throw { error };
+        }
+
     }
 }
 
